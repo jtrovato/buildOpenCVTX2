0919bd6b57717baad5b99d651acd172fbdcd8432
Joe Trovato <j.trovato@sarcos.com>
2017-08-10 22:02:08 +0000

    editing opencv script to work for our project

--- buildOpenCV.sh
+++ buildOpenCV.sh
@@ -14,59 +14,69 @@ sudo apt-get install -y \
     libswscale-dev \
     libeigen3-dev \
     libtbb-dev \
-    libgtk2.0-dev \
+    libgtk-3-dev \
     cmake \
-    pkg-config
+    pkg-config \
+    build-essential \
+    libatlas-base-dev \
+    gfortran \
+    
 
 # Python 2.7
-sudo apt-get install -y python-dev python-numpy python-py python-pytest -y
+sudo apt-get install -y python2.7-dev python3.5-dev python-numpy python-py python-pytest -y
 
-git clone https://github.com/opencv/opencv.git
-cd opencv
-git checkout -b v3.2.0 3.2.0
-# This is for the test data
-cd $HOME
-git clone https://github.com/opencv/opencv_extra.git
-cd opencv_extra
-git checkout -b v3.2.0 3.2.0
+cd ~
+wget -O opencv.zip https://github.com/Itseez/opencv/archive/3.2.0.zip
+unzip opencv.zip
+wget -O opencv_contrib.zip https://github.com/Itseez/opencv_contrib/archive/3.2.0.zip
+unzip opencv_contrib.zip
 
-cd $HOME/opencv
+
+cd ~/opencv-3.2.0
 mkdir build
 cd build
 # Jetson TX2 
 cmake \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX=/usr/local \
-    -DBUILD_PNG=OFF \
-    -DBUILD_TIFF=OFF \
-    -DBUILD_TBB=OFF \
-    -DBUILD_JPEG=OFF \
-    -DBUILD_JASPER=OFF \
-    -DBUILD_ZLIB=OFF \
-    -DBUILD_EXAMPLES=ON \
-    -DBUILD_opencv_java=OFF \
-    -DBUILD_opencv_python2=ON \
-    -DBUILD_opencv_python3=OFF \
-    -DENABLE_PRECOMPILED_HEADERS=OFF \
-    -DWITH_OPENCL=OFF \
-    -DWITH_OPENMP=OFF \
-    -DWITH_FFMPEG=ON \
-    -DWITH_GSTREAMER=OFF \
-    -DWITH_GSTREAMER_0_10=OFF \
-    -DWITH_CUDA=ON \
-    -DWITH_GTK=ON \
-    -DWITH_VTK=OFF \
-    -DWITH_TBB=ON \
-    -DWITH_1394=OFF \
-    -DWITH_OPENEXR=OFF \
-    -DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda-8.0 \
-    -DCUDA_ARCH_BIN=6.2 \
-    -DCUDA_ARCH_PTX="" \
-    -DINSTALL_C_EXAMPLES=ON \
-    -DINSTALL_TESTS=ON \
-    -DOPENCV_TEST_DATA_PATH=../opencv_extra/testdata \
-    -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-3.2.0/modules
+    -D CMAKE_BUILD_TYPE=Release \
+    -D CMAKE_INSTALL_PREFIX=/usr/local \
+    -D BUILD_PNG=OFF \
+    -D BUILD_TIFF=OFF \
+    -D BUILD_TBB=OFF \
+    -D BUILD_JPEG=OFF \
+    -D BUILD_JASPER=OFF \
+    -D BUILD_ZLIB=OFF \
+    -D BUILD_EXAMPLES=ON \
+    -D BUILD_opencv_java=OFF \
+    -D BUILD_opencv_python2=ON \
+    -D BUILD_opencv_python3=OFF \
+    -D ENABLE_PRECOMPILED_HEADERS=OFF \
+    -D WITH_OPENCL=OFF \
+    -D WITH_OPENMP=OFF \
+    -D WITH_FFMPEG=ON \
+    -D WITH_GSTREAMER=OFF \
+    -D WITH_GSTREAMER_0_10=OFF \
+    -D WITH_CUDA=ON \
+    -D WITH_GTK=ON \
+    -D WITH_VTK=OFF \
+    -D WITH_TBB=ON \
+    -D WITH_1394=ON \
+    -D WITH_OPENEXR=OFF \
+    -D CUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda-8.0 \
+    -D CUDA_ARCH_BIN=6.2 \
+    -D CUDA_ARCH_PTX="" \
+    -D INSTALL_PYTHON_EXAMPLES=OFF\
+    -D INSTALL_C_EXAMPLES=OFF \
+    -D INSTALL_TESTS=ON \
+    -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-3.2.0/modules \
     ../
 
+# delete condition that disables dnn if on AARCH64 system
+sed -i '4d' ~/opencv_contrib-3.2.0/modules/dnn/CMakeLists.txt
+cmake ..
+
 # Consider using all 6 cores; $ sudo nvpmodel -m 2 or $ sudo nvpmodel -m 0
-make -j6
+make -j4
+sudo make install
+
+sudo rm /opt/ros/kinetic/lib/python2.7/dist-packages/cv2.so
+sudo ln -s /usr/local/lib/python2.7/dist_packages/cv2.so /opt/ros/kinetic/lib/python2.7/dist-packages/cv2.so
